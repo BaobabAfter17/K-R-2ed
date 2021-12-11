cb087858213e08f9fb0929e7b6baab87dc74fde3
Arthur Yao <yakunyao@gmail.com>
2021-12-11 12:09:39 -0600

    ex3-5

--- Chapter-3/Ex3-5.c
+++ Chapter-3/Ex3-5.c
@@ -2,18 +2,41 @@
 
 #define SIZELIMIT 1000
 
-void itoa(int n, char s[]);
+void itob(int n, char s[], int b);
 void reverse(char s[]);
 
+/* test itob */
 int main()
 {
+    int n = 9090;
+    int b = 16;
+    char s[SIZELIMIT];
 
+    itob(n, s, b);
+    printf("%d convert to: %s\n", n, s);
 }
 
 /* itob: convert n into a base b string representation */
 void itob(int n, char s[], int b)
 {
-    
+    int i, c, sign;
+
+    if ((sign = n) < 0) /* record sign */
+        n = -n;         /* make n positive */
+
+    i = 0;
+    do {    /* generate digits in reverse order */
+        if ((c = n % b) > 9)
+            c = c - 10 + 'a';
+        else
+            c = c + '0';
+        s[i++] = c; /* get next digit */
+    } while ((n /= b) > 0);   /* delete it */
+
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
 }
 
 
