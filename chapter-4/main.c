119b50c144cb23dbcb143d6bdf99d7aed39166e2
Arthur Yao <yakunyao@gmail.com>
2021-12-16 23:47:23 -0600

    ex4-5

--- chapter-4/main.c
+++ chapter-4/main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
-#define MAXLINE 100
+#include <string.h>
 
 /* test strrindex */
 int main()
 {
-    double atof(char s[]);
-
-    printf("%f", atof("123.45e-6"));
+    printf("%d", EOF);
+    printf("%d", islower(EOF));
 }
