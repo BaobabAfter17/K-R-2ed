1243c798114d8c526be85fd9f934b05476c37423
Arthur Yao <yakunyao@gmail.com>
2021-12-24 16:14:57 -0600

    ex5-7

---
+++ chapter-5/solutions/ex5-7.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+#define MAXLEN 1000 /* maximum length of line */
+#define MAXSTOR 5000    /* size of available storage space */
+
+int my_getline(char *, int);
+
+/* readlines: read input lines */
+int readlines(char *lineptr[], char *linestor, int maxlines)
+{
+    int len, nlines;
+    char line[MAXLEN];
+    char *p = linestor;
+    char *linestop = linestor + MAXSTOR;
+
+    nlines = 0;
+    while ((len = my_getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || p + len > linestop)
+            return -1;
+        else {
+            line[len - 1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+            p += len;
+        }
+    return nlines;
+}
